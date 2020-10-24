4c7e5ca2789d3a945520e41c7c78fbb2eb162d02
zARODz11z <andrewrod003@gmail.com>
2020-10-23 21:14:22 -0700

    updated activity_main.xml defined data object able to query parse object

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("XJZBMXhxp00xXtwrWEDMMjlSO1IzLnauW9TMtnTJ")
